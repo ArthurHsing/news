50d4362d2ef1040e77ed0c7a8b670764e1fa3626
幸成 <573105494@qq.com>
2020-02-19 15:55:25 +0800

    新闻系统完毕

--- routes/index.js
+++ routes/index.js
@@ -49,7 +49,52 @@ router.post('/save_add.html', function(req, res){
   list.time = new Date().toLocaleString();
   list.hits = 1;
   list.save(function(){
-    res.send('发布成功');
+    res.send('<script>alert("发布成功"); location.href="/list.html";</script>');
+  });
+});
+
+// 挂载一个保存修改数据的路由save_edit.html
+router.post('/save_edit.html', function(req, res){
+    // 接收客户端传过来的POST方式的数据
+    var title = req.body.title;
+    var author = req.body.author;
+    var from = req.body.from;
+    var content = req.body.content;
+    var id = req.body.id;
+    listModel.findById(id).exec(function(err, data){
+      data.title = title;
+      data.author = author;
+      data.from = from;
+      data.content = content;
+      data.save(function(err){
+        if (err){
+          res.send("<script>alert('修改失败'); location.href = '/list.html';</script>");
+        } else {
+          res.send("<script>alert('修改成功'); location.href = '/list.html';</script>");
+        }
+      });
+    });
+});
+
+// 挂载一个删除新闻的路由/edit.html
+router.get('/edit.html', function(req, res){
+  var id = req.query.id;
+  listModel.findById(id).exec(function(err, data){
+    res.render('newsedit.ejs', {news:data});
+  });
+});
+
+// 挂载一个删除新闻的路由/del.html
+router.get('/del.html', function(req, res){
+  // 接收数据
+  var id = req.query.id;
+
+  // 通过ID查找数据
+  listModel.findById(id).exec(function(err, data){
+    // 找到对象以后执行删除方法
+    data.remove(function(){
+      res.send('<script>alert("删除成功"); location.href="list.html";</script>');
+    });
   });
 });
 
